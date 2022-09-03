b27bfac5968bf98df2a723b588bdb20dcd4b8051
MuhamadIffat <iffatlathoif@gmail.com>
2022-09-03 07:28:59 +0700

    [Auth]Styling Login Page

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  loginForm: FormGroup;
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.initForm();
   }
 
+  initForm(){
+    this.loginForm = this.formBuilder.group({
+      username: ['', [Validators.required]],
+      password: ['', [Validators.required, Validators.minLength(5)]],
+    });
+  }
+
+  onLogin() {
+    console.log(this.loginForm);
+    if(this.loginForm.invalid) {
+      alert('invalid login');
+      return;
+    }
+
+    const username: string = this.loginForm.get('username').value;
+    const password: string = this.loginForm.get('password').value;
+    alert(`login berhasil dengan user:${ username} pass: ${password}`);
+  }
 }
